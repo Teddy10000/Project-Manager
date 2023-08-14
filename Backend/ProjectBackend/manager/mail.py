87544f9c4b974c3b9309b6ea222de90cdb02464b
Teddy10000 <tedakrong12@gmail.com>
2023-08-14 16:17:24 +0000

    worked on the async email sending functionalities

--- Backend/ProjectBackend/manager/mail.py
+++ Backend/ProjectBackend/manager/mail.py
@@ -1,30 +1,43 @@
-from django.core.mail import send_mail
-from django.template.loader import render_to_string 
+import threading
+from threading import Thread
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from django.core.mail import send_mail
 
 from_email = 'projectmanagerworldwide100@gmail.com'
-recipient_list = ['Akrongkofi@gmail.com'] 
 
+class EmailThread(threading.Thread):
+    def __init__(self, subject, html_content, recipient_list):
+        self.subject = subject
+        self.recipient_list = recipient_list
+        self.html_content = html_content
+        threading.Thread.__init__(self)
 
-## THIS FUNCTION RENDERS THE HTML TO STRING
-def rendering_to_string(url1,url2,header,message):
+    def run(self):
+        msg = EmailMessage(self.subject, self.html_content, from_email, self.recipient_list)
+        msg.content_subtype = "html"
+        msg.send()
+
+def rendering_to_string(url1, url2, header, message):
     email_template = 'account_created.html'
     html_message = render_to_string(email_template, {
-    'header': header,
-    'message':message,
-    'links': [
-        {'name': 'Link 1', 'url': url1},
-        {'name': 'Link 2', 'url': url2},
-    ], 
-    }) 
+        'header': header,
+        'message': message,
+        'links': [
+            {'name': 'Link 1', 'url': url1},
+            {'name': 'Link 2', 'url': url2},
+        ],
+    })
     text_content = strip_tags(html_message)
-    
-    return html_message , text_content
 
+    return html_message, text_content
 
-def send_email(subject, msg ,Header ,recipient):
-    with_tags ,message = rendering_to_string('www.127/00/00','www.127/003', Header, msg)
-    send_mail(subject, message,from_email, recipient ,html_message=with_tags )
-    return print("Email sent successfully!") 
+def send_email(subject, msg, header, recipient):
+    with_tags, message = rendering_to_string('www.127/00/00', 'www.127/003', header, msg)
+    EmailThread(subject, with_tags, recipient).start()
+    return print("Email sending initiated!")
 
-
+# Example usage
+#recipient_list = ['Akrongkofi@gmail.com']
+#send_email("Test Subject", "Hello, this is a test message.", "Test Header", recipient_list)
