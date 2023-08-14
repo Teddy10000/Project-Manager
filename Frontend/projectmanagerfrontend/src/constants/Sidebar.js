7f8bc16cc86f128078bc88f0553585683c3fcf0f
Teddy10000 <tedakrong12@gmail.com>
2023-08-14 17:39:01 +0000

    worked on the sidebar

--- Frontend/projectmanagerfrontend/src/constants/Sidebar.js
+++ Frontend/projectmanagerfrontend/src/constants/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaBars, FaTimes , FaCog, FaClipboardList,  FaChartLine,FaProjectDiagram,FaTasks,FaCalendarAlt,FaUsersCog } from 'react-icons/fa';
 import TotalTasksChart from './TotalTaskChart';
 import ProjectSummaryChart from './Project-summary';
@@ -7,13 +7,36 @@ import TasksChart from './Taskschart';
 import ProjectScreen from './Project-list';
 import NewDashboard from './NewDashboard';
 import { Link } from 'react-router-dom';
+import api from '../apis/api-auth';
+import { USER_DETAILED_URL } from '../utilities/constant';
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [user , setUser] = useState('')
 
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
 
-  };
+  };  
+  function generateRandomAvatarUrl() {
+	const randomSeed = Math.floor(Math.random() * 1000) + 1;
+	return `https://i.pravatar.cc/150?u=${randomSeed}`;
+  }
+
+const avatarUrl = generateRandomAvatarUrl()
+  useEffect(() => {
+	const fetchUserInfo = async () => {
+		try{
+		  const response = 	await api.get(`${USER_DETAILED_URL}`)
+		  if (response.data){
+			setUser(response.data)
+		  }
+		}
+		catch(err){
+			throw new Error('Please login')
+		}
+	};
+	fetchUserInfo();
+  },[])
   
   return (
 
@@ -32,72 +55,76 @@ const Sidebar = () => {
 					<span className="text-xs font-normal text-content2">Manage your project better</span>
 				</div>
 			</section> 
-      <div className="divider my-0"></div>
+      			<div className="divider my-0"></div>
 			<section className="sidebar-content mt-16">
 				<nav className="menu rounded-md">
 					<section className="menu-section px-4">
 						<span className="menu-title">Main menu</span>
-						<ul className="menu-items ">
-              <li className="menu-item text-xl my-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 3h18v18H3zm4 6h4m-4 4h4m-4 4h4m6-10h4m-4 4h4m-4 4h4"
-              />
-              </svg>
-								<span>Dashboard</span>
-							</li>
-          
-							<li className="menu-item text-xl my-2">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-                  />
-                </svg>
+								<ul className="menu-items "> 
+								<Link to={'/'}>
+									<li className="menu-item text-xl my-2">
+									<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-5 w-5 opacity-75"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+									strokeWidth="2"
+									>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M3 3h18v18H3zm4 6h4m-4 4h4m-4 4h4m6-10h4m-4 4h4m-4 4h4"
+									/>
+									</svg>
+										<span>Dashboard</span>
+									</li> 
+							</Link>
+							
+							<Link to={'/project'}>
+								<li className="menu-item text-xl my-2">
+									<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
+									/>
+									</svg>
 								<span>Projects</span>
-							</li>
-              <li className="menu-item text-xl my-2">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none"  viewBox="0 0 24 24" stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                    />
-                  </svg>
-								<span>Tasks</span>
-							</li>
+								</li>
+							</Link>
+							<Link to={'/tasks'}>
+								<li className="menu-item text-xl my-2">
+									<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none"  viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+										<path strokeLinecap="round" strokeLinejoin="round" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"/>
+									</svg>
+										<span>Tasks</span>
+									</li>
+							</Link>
+							<Link to={"/teams"}>
 							<li className="menu-item menu-active text-xl my-4">
 								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
 									<path strokeLinecap="round" strokeLinejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
 								</svg>
 								<span>Teams</span>
 							</li>
-              <li className="menu-item text-xl my-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M20 4v6m0 0v6m0-6h-6m6 0H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2z" />
-              </svg>
-								<span>Project Milestone</span>
+							</Link>
+							<Link to={"/milestones"}>
+							<li className="menu-item text-xl my-2">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+								<path strokeLinecap="round" strokeLinejoin="round" d="M20 4v6m0 0v6m0-6h-6m6 0H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2z" />
+							</svg>
+									<span>Project Milestone</span>
 							</li>
-              <li className="menu-item text-xl my-2">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M20 4v6m0 0v6m0-6h-6m6 0H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2z" />
-              </svg>
+							</Link>
+							<Link to={'/issues'}>
+							<li className="menu-item text-xl my-2">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+									<path strokeLinecap="round" strokeLinejoin="round" d="M20 4v6m0 0v6m0-6h-6m6 0H4a2 2 0 00-2 2v10a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2z" />
+								</svg>
 								<span>Issues</span>
 							</li>
+							</Link>
 		
 							<li className="">
 								<input type="checkbox" id="menu-1" className="menu-toggle" />
@@ -136,11 +163,11 @@ const Sidebar = () => {
 					<label className="whites mx-2 flex h-fit w-full cursor-pointer p-0 hover:bg-gray-4" tabIndex="0">
 						<div className="flex flex-row gap-4 p-4">
 							<div className="avatar-square avatar avatar-md">
-								<img src="https://i.pravatar.cc/150?img=30" alt="avatar" />
+								<img src={avatarUrl}  alt="avatar" />
 							</div>
 
 							<div className="flex flex-col">
-								<span>Sandra Marx</span>
+								<span>{user.name}</span>
 							</div>
 						</div>
 					</label>
